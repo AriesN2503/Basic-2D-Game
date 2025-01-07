96a0660dec8773f6c8a8688178f8d16dcc6015f3
Aries <lehuy25032004@gmail.com>
2025-01-07 21:36:46 +0700

    feat: add game thread

--- src/my2dgame/GamePanel.java
+++ src/my2dgame/GamePanel.java
@@ -9,23 +9,37 @@ import java.awt.Dimension;
  *
  * @author Aries
  */
-public class GamePanel extends JPanel {
-    
-    //SCREEN SETTING
-    final int originalTileSize = 16; //Tile(ô lưới), a tile has a value of 16, mean that it have the width of 16px and the height of 16px
-    final int scale = 3;
-    
-    final int tileSize = originalTileSize * scale; //48px by now
-    
-    final int maxScreenCol = 16; //have 16 tile for width
-    final int maxScreenRow = 12; //have 12 tile for height
+public class GamePanel extends JPanel implements Runnable {
 
-    final int screenWidth = tileSize * maxScreenCol; //768px
-    final int screenHeight = tileSize * maxScreenRow; //576px
+    // SCREEN SETTING
+    final int originalTileSize = 16; // Tile(ô lưới), a tile has a value of 16, mean that it have the width of 16p
+                                     //  and the height of 16px
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48px by now
+
+    final int maxScreenCol = 16; // have 16 tile for width
+    final int maxScreenRow = 12; // have 12 tile for height
+
+    final int screenWidth = tileSize * maxScreenCol; // 768px
+    final int screenHeight = tileSize * maxScreenRow; // 576px
+
+    // THREAD
+    Thread gameThread;
 
     public GamePanel() {
-        this.setPreferredSize(new Dimension(screenWidth, screenHeight)); //set size for this class (JPanel)
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // set size for this class (JPanel)
         this.setBackground(Color.BLACK);
-        this.setDoubleBuffered(true); //Turn on Double Buffer for drawing
+        this.setDoubleBuffered(true); // Turn on Double Buffer for drawing
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this); // the 'this' mean that we passing GamePanel class to this Thread's constructor
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
